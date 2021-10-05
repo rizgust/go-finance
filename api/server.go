b29b131b530717e411b65e37545954677cb17dc7
rizky <rizgust.dev@gmail.com>
2021-10-05 09:44:02 +0700

    [FIX] remove plural for list endpoint

--- api/server.go
+++ api/server.go
@@ -25,32 +25,32 @@ func (server *Server) setupRouter() {
 		{
 			master.POST("/bank", server.createMBank)
 			master.GET("/bank/:id", server.getMBank)
-			master.GET("/banks", server.getListMBank)
+			master.GET("/bank", server.getListMBank)
 			master.PUT("/bank", server.updateMBank)
 			
 			master.POST("/bank-acc", server.createMBankAccount)
 			master.GET("/bank-acc/:id", server.getMBankAccount)
-			master.GET("/bank-accs", server.getListMBankAccount)
+			master.GET("/bank-acc", server.getListMBankAccount)
 			master.PUT("/bank-acc", server.updateMBankAccount)
 
 			master.POST("/book-period", server.createMBookPeriod)
 			master.GET("/book-period/:id", server.getMBookPeriod)
-			master.GET("/book-periods", server.getListMBookPeriod)
+			master.GET("/book-period", server.getListMBookPeriod)
 			master.PUT("/book-period", server.updateMBookPeriod)
 
 			master.POST("/coa", server.createMChartOfAccount)
 			master.GET("/coa/:id", server.getMChartOfAccount)
-			master.GET("/coas", server.getListMChartOfAccount)
+			master.GET("/coa", server.getListMChartOfAccount)
 			master.PUT("/coa", server.updateMChartOfAccount)
 
 			master.POST("/va", server.createMVirtualAccount)
 			master.GET("/va/:id", server.getMVirtualAccount)
-			master.GET("/vas", server.getListMVirtualAccount)
+			master.GET("/va", server.getListMVirtualAccount)
 			master.PUT("/va", server.updateMVirtualAccount)
 
 			master.POST("/discount", server.createMDiscount)
 			master.GET("/discount/:id", server.getMDiscount)
-			master.GET("/discounts", server.getListMDiscount)
+			master.GET("/discount", server.getListMDiscount)
 			master.PUT("/discount", server.updateMDiscount)
 		}	
 		
